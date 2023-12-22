7987f684113411919ca34899a739f3313964dee3
DESKTOP-MAF5T6N\Darky <anandarizky166@gmail.com>
2023-12-22 10:05:35 +0700

    handleSubstraction

--- starter/counter-starter/src/App.js
+++ starter/counter-starter/src/App.js
@@ -3,26 +3,33 @@ import classname from 'classnames';
 
 import shoppingIcon from './assets/shopping-icon.svg';
 import minusIcon from './assets/minus-icon.svg';
-import plusIcon from './assets/plus-icon.svg'
+import plusIcon from './assets/plus-icon.svg';
 
 import './App.css';
 
 function App() {
 	const [value, setValue] = useState('');
-  const [todos, setTodos] = useState([
-    {title: 'Semangka', count: 1},
-    {title: 'Pisang', count: 1},
-    {title: 'Jambu', count: 1},
-  ]);
+	const [todos, setTodos] = useState([
+		{ title: 'Semangka', count: 0 },
+		{ title: 'Pisang', count: 0 },
+		{ title: 'Jambu', count: 0 },
+	]);
 
-  const handleAdditionCount = (index) => {
-    const newTodos = [...todos];
+	const handleAdditionCount = (index) => {
+		const newTodos = [...todos];
 
-    newTodos[index].count = newTodos[index].count + 1;
+		newTodos[index].count = newTodos[index].count + 1;
 
-    setTodos(newTodos);
-  }
+		setTodos(newTodos);
+	};
 
+	const handleSubstractionCount = (index) => {
+		const newTodos = [...todos];
+
+		newTodos[index].count = newTodos[index].count - 1;
+
+		setTodos(newTodos);
+	};
 	// Cara Kesatu
 	// function handleClickPlus() {
 	//   setCount(count + 1);
@@ -31,10 +38,10 @@ function App() {
 	//   setCount(count - 1);
 	// }
 
-  // Cara Ketiga
+	// Cara Ketiga
 	// function MyButton() {
-  //   const [count, setCount] = useState(0);
-    
+	//   const [count, setCount] = useState(0);
+
 	// 	function handleClickPlus() {
 	// 		setCount(count + 1);
 	// 	}
@@ -45,13 +52,13 @@ function App() {
 
 	// 	return (
 	// 		<>
-  //       <h1>{count}</h1>
+	//       <h1>{count}</h1>
 	// 			<button onClick={handleClickPlus}>Tambah</button>
 	// 			<button onClick={handleClickMinus}>Kurang</button>
 	// 		</>
 	// 	);
 	// }
-  
+
 	return (
 		<>
 			{/* Cara Kesatu */}
@@ -62,7 +69,7 @@ function App() {
 			<button onClick={() => {setCount(count + 1)}}>Tambah</button>
       <button onClick={() => {setCount(count - 1)}}>Kurang</button> */}
 
-      {/* Cara Ketiga */}
+			{/* Cara Ketiga */}
 			{/* <MyButton /> */}
 
 			<nav className="nav">
@@ -84,33 +91,41 @@ function App() {
 					</button>
 				</form>
 
-        {todos.length > 0 ? (
-          <div className='todos'>
-          {todos.map((todo, index, arr) => {
-            return (
-							<div key={index} className={`todo ${!(arr.length === index + 1) && 'todo-divider'}`}>
-                {todo.title}
-								<div className="todo-icon-wrapper">
-									<div className="todo-count">{todo.count}</div>
-									<button className="todo-action-button">
-										<img src={minusIcon} alt="Minus Icon" />
-									</button>
-									<button onClick={() => {handleAdditionCount(index)}} className="todo-action-button">
-										<img src={plusIcon} alt="Plus Icon" />
-									</button>
+				{todos.length > 0 ? (
+					<div className="todos">
+						{todos.map((todo, index, arr) => {
+							return (
+								<div key={index} className={`todo ${!(arr.length === index + 1) && 'todo-divider'}`}>
+									{todo.title}
+									<div className="todo-icon-wrapper">
+										<div className="todo-count">{todo.count}</div>
+										<button
+											onClick={() => {
+												handleSubstractionCount(index);
+											}}
+											className="todo-action-button"
+										>
+											<img src={minusIcon} alt="Minus Icon" />
+										</button>
+										<button
+											onClick={() => {
+												handleAdditionCount(index);
+											}}
+											className="todo-action-button"
+										>
+											<img src={plusIcon} alt="Plus Icon" />
+										</button>
+									</div>
 								</div>
-							</div>
-						);
-          })}
-          </div> 
-        ) : (
-          <div>Kosong</div>
-        )}
+							);
+						})}
+					</div>
+				) : (
+					<div>Kosong</div>
+				)}
 			</section>
-    </>
-    
+		</>
 	);
-  
 }
 
 export default App;
