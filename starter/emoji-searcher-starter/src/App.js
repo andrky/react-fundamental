0d1c9a0435045281b17a2c3b53a9cac68745737e
DESKTOP-MAF5T6N\Darky <anandarizky166@gmail.com>
2024-01-04 13:52:38 +0700

    dataFetchingInReact

--- starter/emoji-searcher-starter/src/App.js
+++ starter/emoji-searcher-starter/src/App.js
@@ -1,4 +1,18 @@
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+
 function App() {
+  const [emojisData, SetEmojisData] = useState([]);
+
+  useEffect(() => {
+    async function fetchEmojis() {
+      const res = await axios.get('https://run.mocky.io/v3/5a982f64-218d-45d7-a380-ebe924d55631')
+
+      console.log(res)
+    }
+    fetchEmojis()
+  }, [])
+
   return (
     <div>
       <h1>Hello, world!</h1>
